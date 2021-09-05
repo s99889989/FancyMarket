74dbda740c1e25255fa8b70b459fd8a48c2a5374
Daxton <s02048898067@gmail.com>
2021-09-05 14:17:54 +0800

    1

---
+++ src/main/java/com/daxton/fancymarket/FancyMarket.java
@@ -0,0 +1,17 @@
+package com.daxton.fancymarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FancyMarket extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
